c22bcb5217a35a50eccd9428a020d6f99b19d06e
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2019-12-21 14:24:53 -0800

    Make responsive

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+/*
+ * Toggle navigation bars
+ */
+
+function toggleClass(element, toggleClass) {
+  if (element.classList) {
+    element.classList.toggle(toggleClass);
+  } else {
+    // For IE9
+    var classes = element.className.split(" ");
+    var i = classes.indexOf(toggleClass);
+
+    if (i >= 0) {
+      classes.splice(i, 1);
+    } else {
+      classes.push(toggleClass);
+    }
+
+    element.className = classes.join(' ');
+  }
+}
+
+var siteNavToggle = document.getElementById('site-nav-toggle');
+var navList = document.getElementById('site-navigation').getElementsByTagName('ul')[0];
+
+siteNavToggle.onclick = function() {
+  toggleClass(siteNavToggle, 'opened');
+  toggleClass(navList, 'opened');
+}
+
+var secondaryNavToggle = document.getElementById('secondary-nav-toggle');
+var pageSidebar = document.getElementById('page-sidebar');
+
+secondaryNavToggle.onclick = function() {
+  toggleClass(secondaryNavToggle, 'opened');
+  toggleClass(pageSidebar, 'opened');
+}
