4e88547f94ba870fe9c412d09302f1fc12fe909d
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2019-12-23 11:02:36 -0800

    Toggle dropdown on click instead of hover; correct spacing; add heading to documentation layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-/*
- * Toggle navigation bars
- */
-
 function toggleClass(element, toggleClass) {
   if (element.classList) {
     element.classList.toggle(toggleClass);
@@ -20,6 +16,25 @@ function toggleClass(element, toggleClass) {
   }
 }
 
+function removeClass(element, removeClass) {
+  if (element.classList) {
+    element.classList.remove(removeClass);
+  } else {
+    // For IE9
+    var classes = element.className.split(" ");
+    var i = classes.indexOf(removeClass);
+
+    if (i >= 0) {
+      classes.splice(i, 1);
+      element.className = classes.join(' ');
+    }
+  }
+}
+
+/*
+ * Toggle navigation bars for mobile
+ */
+
 var siteNavToggle = document.getElementById('site-nav-toggle');
 var navList = document.getElementById('site-navigation').getElementsByTagName('ul')[0];
 
@@ -37,3 +52,22 @@ if(secondaryNavToggle) {
     toggleClass(pageSidebar, 'opened');
   }
 }
+
+/*
+ * Toggle site navigation dropdown menus
+ */
+
+var siteNavItems = document.getElementById('site-navigation').getElementsByTagName('li');
+Array.from(siteNavItems).forEach(function(item, index) {
+  item.addEventListener('click', function(event) {
+    // Close all other dropdown menus
+    for (var i = 0; i < siteNavItems.length; i++) {
+      if ( i != index ) {
+        removeClass(siteNavItems[i], 'opened');
+      }
+    }
+
+    // Toggle clicked dropdown menu
+    toggleClass(item, 'opened');
+  });
+});
